0aba92ab29803ceb5da9a0ecbb8eaa2214dabfb1
Goutam Sarkar <goutams@google.com>
2020-10-07 18:25:19 +0530

    New Python code

---
+++ car_driving.py
@@ -0,0 +1,13 @@
+# Eligible for a car driving. 
+
+name = input("Enter Driver Name: ")
+age = int(input("Enter Age: "))
+
+if age >= 18:
+    print(f"{name} is {age} years old now can drive a car.")
+else:
+    print(f"{name} can not drive a car.")
+
+
+
+
